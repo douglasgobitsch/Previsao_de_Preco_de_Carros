92a942e1588c848a1e1a7f324f639926d2fc92e8
Douglas Gobitsch <101853976+douglasgobitsch@users.noreply.github.com>
2024-09-13 01:41:13 -0300

    Update tratamento_dados.py

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -1,7 +1,23 @@
-import pandas as pd
+import dask.dataframe as dd
 
-df = pd.read_csv('dataset_cars.csv')
+# Ler o CSV forçando todas as colunas como string
+df = dd.read_csv('dataset_cars.csv', dtype=str, low_memory=False)
 
-df.fillna(0, inplace=True)
+# Definir colunas a serem convertidas
+#colunas_float = ['coluna_x', 'coluna_y']  # Coloque os nomes das colunas que devem ser float
+#colunas_int = ['coluna_z']  # Coloque os nomes das colunas que devem ser int
 
-df.to_csv('dataset_cars_novo.csv', index=False)
+# Preencher valores nulos para colunas que serão convertidas
+#for col in colunas_float + colunas_int:
+#    df[col] = df[col].fillna('')  # Preencher valores nulos com string vazia
+
+# Converter para float e int
+#for col in colunas_float:
+#    df[col] = df[col].astype(float, errors='coerce')  # Convertendo para float, forçando erros para NaN
+
+#for col in colunas_int:
+#    df[col] = df[col].astype(float, errors='coerce')  # Primeiro converter para float para lidar com valores nulos
+#    df[col] = df[col].fillna(0).astype(int)  # Preencher NaN com 0 e converter para int
+
+# Salvar o novo CSV
+df.to_csv('dataset_cars_novo.csv', single_file=True, index=False)
